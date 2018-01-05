4d6cc47891a03dde84b741e6700d97c7e974dfce
Administrator <Administrator@t450-PC>
2018-01-05 15:14:18 +0800

    添加单元测试功能

---
+++ src/test/java/com/xiaoshabao/vkan/service/impl/FileManagerServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.xiaoshabao.vkan.service.impl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.xiaoshabao.vkan.App;
+import com.xiaoshabao.vkan.service.FileManagerService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@WebAppConfiguration
+public class FileManagerServiceImplTest {
+
+	@Autowired
+	private FileManagerService fileService;
+
+	@Test
+	public void testAddProject() {
+		try {
+			this.fileService.addProject("测试项目", "E:\\vm\\test\\test");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+		
+	}
+
+}
