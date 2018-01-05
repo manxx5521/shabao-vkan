8f24811d36ee08d0cc671f77f51309bb784fe9bb
Administrator <Administrator@t450-PC>
2018-01-05 15:16:10 +0800

    记录

---
+++ src/main/resources/sql/vk_tag.sql
@@ -0,0 +1,43 @@
+
+-- ----------------------------
+-- Records of vk_tag
+-- ----------------------------
+INSERT INTO `vk_tag` VALUES ('1001', '区域', null, '1', '1', '0', '0', '1', '1', '1', null);
+INSERT INTO `vk_tag` VALUES ('1002', '类型', null, '1', '1', '0', '0', '1', '1', '2', null);
+INSERT INTO `vk_tag` VALUES ('1003', '首字母', null, '1', '1', '0', '0', '1', '1', '3', null);
+INSERT INTO `vk_tag` VALUES ('1004', '名字', null, '1', '1', '0', '0', '1', '1', '4', null);
+INSERT INTO `vk_tag` VALUES ('1005', '关键词', null, '1', '1', '0', '0', '1', '1', '5', null);
+INSERT INTO `vk_tag` VALUES ('1006', '星级', null, '1', '1', '0', '0', '1', '1', '6', null);
+INSERT INTO `vk_tag` VALUES ('1007', '质量', null, '1', '1', '0', '0', '1', '1', '7', null);
+INSERT INTO `vk_tag` VALUES ('1008', '类型1', null, '1', '1', '0', '0', '1', '1', '8', null);
+INSERT INTO `vk_tag` VALUES ('1009', '类型2', null, '1', '1', '0', '0', '1', '1', '9', null);
+INSERT INTO `vk_tag` VALUES ('2001', '国产', null, '1', '2', '0', '1001', '1', '1', '1', null);
+INSERT INTO `vk_tag` VALUES ('2002', '日韩', null, '1', '2', '0', '1001', '1', '1', '2', null);
+INSERT INTO `vk_tag` VALUES ('2003', 'A', null, '1', '2', '0', '1003', '1', '1', '1', null);
+INSERT INTO `vk_tag` VALUES ('2004', 'B', null, '1', '2', '0', '1003', '1', '1', '2', null);
+INSERT INTO `vk_tag` VALUES ('2005', 'C', null, '1', '2', '0', '1003', '1', '1', '3', null);
+INSERT INTO `vk_tag` VALUES ('2006', 'D', null, '1', '2', '0', '1003', '1', '1', '4', null);
+INSERT INTO `vk_tag` VALUES ('2007', 'E', null, '1', '2', '0', '1003', '1', '1', '5', null);
+INSERT INTO `vk_tag` VALUES ('2008', 'F', null, '1', '2', '0', '1003', '1', '1', '6', null);
+INSERT INTO `vk_tag` VALUES ('2009', 'G', null, '1', '2', '0', '1003', '1', '1', '7', null);
+INSERT INTO `vk_tag` VALUES ('2010', 'H', null, '1', '2', '0', '1003', '1', '1', '8', null);
+INSERT INTO `vk_tag` VALUES ('2011', 'I', null, '1', '2', '0', '1003', '1', '1', '9', null);
+INSERT INTO `vk_tag` VALUES ('2012', 'J', null, '1', '2', '0', '1003', '1', '1', '10', null);
+INSERT INTO `vk_tag` VALUES ('2013', 'K', null, '1', '2', '0', '1003', '1', '1', '11', null);
+INSERT INTO `vk_tag` VALUES ('2014', 'L', null, '1', '2', '0', '1003', '1', '1', '12', null);
+INSERT INTO `vk_tag` VALUES ('2015', 'M', null, '1', '2', '0', '1003', '1', '1', '13', null);
+INSERT INTO `vk_tag` VALUES ('2016', 'N', null, '1', '2', '0', '1003', '1', '1', '14', null);
+INSERT INTO `vk_tag` VALUES ('2017', 'O', null, '1', '2', '0', '1003', '1', '1', '15', null);
+INSERT INTO `vk_tag` VALUES ('2018', 'P', null, '1', '2', '0', '1003', '1', '1', '16', null);
+INSERT INTO `vk_tag` VALUES ('2019', 'Q', null, '1', '2', '0', '1003', '1', '1', '17', null);
+INSERT INTO `vk_tag` VALUES ('2020', 'R', null, '1', '2', '0', '1003', '1', '1', '18', null);
+INSERT INTO `vk_tag` VALUES ('2021', 'S', null, '1', '2', '0', '1003', '1', '1', '19', null);
+INSERT INTO `vk_tag` VALUES ('2022', 'T', null, '1', '2', '0', '1003', '1', '1', '20', null);
+INSERT INTO `vk_tag` VALUES ('2023', 'U', null, '1', '2', '0', '1003', '1', '1', '21', null);
+INSERT INTO `vk_tag` VALUES ('2024', 'V', null, '1', '2', '0', '1003', '1', '1', '22', null);
+INSERT INTO `vk_tag` VALUES ('2025', 'W', null, '1', '2', '0', '1003', '1', '1', '23', null);
+INSERT INTO `vk_tag` VALUES ('2026', 'X', null, '1', '2', '0', '1003', '1', '1', '24', null);
+INSERT INTO `vk_tag` VALUES ('2027', 'Y', null, '1', '2', '0', '1003', '1', '1', '25', null);
+INSERT INTO `vk_tag` VALUES ('2028', 'Z', null, '1', '2', '0', '1003', '1', '1', '26', null);
+INSERT INTO `vk_tag` VALUES ('2029', '未知', null, '1', '2', '0', '1003', '1', '1', '27', null);
+INSERT INTO `vk_tag` VALUES ('2030', '欧美', null, '1', '2', '0', '1001', '1', '1', '3', null);
